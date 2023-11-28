ba7df365c39305ff3cfadcab8c7b89a1d553bd0d
Efe Aslan <108073677+xefe1907x@users.noreply.github.com>
2023-11-28 16:58:42 +0300

    Setup scene, added bars and bars are rotated constantly

--- Assets/Scripts/Behavior/SquareBarBehavior.cs
+++ Assets/Scripts/Behavior/SquareBarBehavior.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SquareBarBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float rotationSpeed = 5.5f;
+
     void Start()
     {
-        
+        GetIndexInHierarchy();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() => RotateSquareBarConstantly();
+
+    void RotateSquareBarConstantly()
     {
-        
+        transform.Rotate(-Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+    
+    void GetIndexInHierarchy()
+    {
+        int objectIndex = transform.GetSiblingIndex() + 1;
+
+        switch (objectIndex)
+        {
+            case 1:
+                rotationSpeed = 1.5f;
+                break;
+            case 2:
+                rotationSpeed = 1.7f;
+                break;
+            case 3:
+                rotationSpeed = 1.9f;
+                break;
+            case 4:
+                rotationSpeed = 2.1f;
+                break;
+            case 5:
+                rotationSpeed = 2.3f;
+                break;
+            case 6:
+                rotationSpeed = 2.5f;
+                break;
+            case 7:
+                rotationSpeed = 2.7f;
+                break;
+            case 8:
+                rotationSpeed = 2.9f;
+                break;
+        }
     }
 }
